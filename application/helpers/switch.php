0d98e9cbbbf4bc9050adbcf3ba9f6715b32987c9
ScholarQAQ <fallen_scholar@163.com>
2022-07-16 10:54:15 +0800

    feat(Helpers):增加辅助函数 公共函数和转换函数文件

---
+++ application/helpers/switch.php
@@ -0,0 +1,39 @@
+<?php
+
+if (!function_exists('to_under_score')) {
+    /**
+     * Function to_under_score
+     * 驼峰命名转下划线命名
+     *
+     * @param string $str
+     * @return string
+     */
+    function to_under_score(string $str): string
+    {
+        $dStr = preg_replace_callback('/([A-Z]+)/',function($match) {
+            return '_'.strtolower($match[0]);
+        }, $str);
+        return trim(preg_replace('/_{2,}/','_',$dStr),'_');
+    }
+}
+
+if (!function_exists('to_camel_case')) {
+    /**
+     * Function to_camel_case
+     * 下划线命名转驼峰命名
+     * @param string $str
+     * @return string
+     */
+    function to_camel_case(string $str): string
+    {
+        $array = explode('_', $str);
+        $result = $array[0];
+        $len=count($array);
+        if($len>1) {
+            for($i=1;$i<$len;$i++) {
+                $result.= ucfirst($array[$i]);
+            }
+        }
+        return $result;
+    }
+}
